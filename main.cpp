eefab0c7f61924d4f8f1b3c86d21adb3d9a5c8b3
hayamo <hayamoproductions@gmail.com>
2014-12-02 15:41:32 -0800

    filled out reshape, init, and main

--- main.cpp
+++ main.cpp
@@ -5,7 +5,8 @@
 
 void init(void)
 {
-
+	glClearColor (0.0, 0.0, 0.0, 0.0);
+	glShadeModel (GL_FLAT);
 }
 
 void display(void)
@@ -13,17 +14,34 @@ void display(void)
 
 }
 
-void reshape(void)
+void reshape(int w, int h)
 {
-
+	glViewport (0, 0, (GLsizei) w, (GLsizei) h);
+	glMatrixMode (GL_PROJECTION);
+	glLoadIdentity();
+	gluPerspective(65.0, (GLfloat) w/(GLfloat) h, 1.0, 20.0);
+	glMAtrixMode(GL_MODELVIEW);
+	glLoadIdentity();
 }
 
-void keyboard()
+void keyboard(unsigned char key, int x, int y)
 {
-
+	case 27:
+		exit(0);
+		break;
+	default:
+		break;
 }
 
-int main()
+int main(int argc, char** argv)
 {
-
+	glutInit(&argc, argv);
+	glutInitDisplayMode (GLUT_SINGLE | GLUT_RGB);
+	glutInitWindowSize (500, 500);
+	glutCreateWindow (argv[0]);
+	init ();
+	glutDisplayFunc(display);
+	glutReshapeFunc(reshape);
+	glutMainLoop();
+	return 0;
 }
