55c72d170a52399241a3e9f448f9dea331795f54
hayamo <hayamoproductions@gmail.com>
2014-12-02 15:32:52 -0800

    created generic main.cpp

---
+++ main.cpp
@@ -0,0 +1,29 @@
+#include <GL/GL.h>
+#include <GL/GLU.h>
+#include <GL/glut.h>
+#include <stdlib.h>
+
+void init(void)
+{
+
+}
+
+void display(void)
+{
+
+}
+
+void reshape(void)
+{
+
+}
+
+void keyboard()
+{
+
+}
+
+int main()
+{
+
+}
